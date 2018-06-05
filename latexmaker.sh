87e4ce2645f6c9d9c4ec9a10da66a43d64f0b5ee
Timothee Simon <xorob0@posteo.net>
2018-06-05 17:32:18 +0200

    Reworked most of the code to make it smaller and easier to read

--- latexmaker.sh
+++ latexmaker.sh
@@ -5,58 +5,30 @@
 #
 FILENAME=$1
 DIRECTORY=`pwd`
-LATEXCMD="/usr/bin/latexdiff"
+LATEXDIFFCMD="/usr/bin/latexdiff"
+LATEXCMD="/usr/bin/xelatex"
 FILE="$DIRECTORY/$FILENAME"
-ORIGINAL="$DIRECTORY/.original"
-TEX_DIFF="$DIRECTORY/diff"
-EXT="tex"
+ORIGINAL="/tmp/Latex_Diff/original.tex"
+DIFF="/tmp/Latex_Diff/diff.tex"
+CHECK=`md5sum $FILE.$EXT`
+TIME=1
+
+cp $FILE.$EXT $ORIGINAL.$EXT
 
 while true
 do
 
 	#
-	# If original does not exist create one
-	#
-	if ! [ -a $ORIGINAL.$EXT ]
-	then
-		cp $FILE.$EXT $ORIGINAL.$EXT
-		md5sum $ORIGINAL.$EXT | awk '{print $1}' > $ORIGINAL.md5
-	fi
-
-	#
-	# If diff pdf does not exist create it
-	#
-	if ! [ -a $TEX_DIFF.$EXT ]
-	then
-		cp $FILE.$EXT $TEX_DIFF.$EXT
-		pdflatex $TEX_DIFF.$EXT
-	fi
-
-	#
-	# If the current MD5 does not exist, create it
-	#
-	if ! [ -a $FILE.md5 ]
-	then
-		md5sum $FILE.$EXT | awk '{print $1}' > $FILE.md5
-	fi
-
-	#
-	# Create and assignate md5 variables
-	#
-	OLD_MD5=$(cat $FILE.md5 | awk '{print $1}')
-#	echo "$OLD_MD5"
-	NEW_MD5=$(md5sum $FILE.$EXT | awk '{print $1}')
-#	echo "$NEW_MD5"
-
-	#
 	# If the file has changed, change the md5 and create a new diff pdf
 	#
-	if ! [ $OLD_MD5 = $NEW_MD5 ]
+	CHECKNOW=`md5sum $FILE`
+
+	if ! [ $CHECK = $CHECKNOW ]
 	then
-		echo $NEW_MD5 > $FILE.md5
-		latexdiff $ORIGINAL.$EXT $FILE.$EXT > $TEX_DIFF.$EXT
-		pdflatex $TEX_DIFF.$EXT
-		pdflatex $FILE.$EXT
+		CHECK=$CHECKNOW
+
+		$LATEXDIFFCMD $ORIGINAL $FILE > $DIFF
+		$LATEXCMD $DIFF --output-directory $DIRECTORY
 	fi
-	sleep 1
+	sleep $TIME
 done
