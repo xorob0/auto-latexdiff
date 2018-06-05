7a724b2b48eed4fb85e5d3ab31cd29916c5cc0c8
Timothee Simon <xorob0@posteo.net>
2018-06-05 18:11:27 +0200

    Renamed even the script

--- Automatic_Latex_Differential.sh
+++ Automatic_Latex_Differential.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-# Defining a few variables...
+## Defining a few variables...
 
+# Files variables
 FILENAME=$1
 DIRECTORY=`pwd`
 FILE="$DIRECTORY/$FILENAME"
-TMPDIRECTORY='/tmp/Latex_Diff'
 
+TMPDIRECTORY='/tmp/Latex_Diff'
 ORIGINAL="$TMPDIRECTORY/original.tex"
 DIFF="$TMPDIRECTORY/diff.tex"
 
+# Executables
 LATEXDIFFCMD="/usr/bin/latexdiff"
 LATEXCMD="/usr/bin/xelatex -interaction nonstopmode"
 
+# Time between refresh
 TIME=1
 
+# Generating temporary directory
 mkdir -p $TMPDIRECTORY
 cp $FILE $ORIGINAL
 
+# Deleting temporary directory on exit
 trap printout SIGINT
 printout() {
 	rm -Rf $TMPDIRECTORY
-	echo "Done"
 	exit
 }
 
+# Infinite loop
 while :
 do
 
-	# If the file has changed, change the md5 and create a new diff pdf
-
 	CHECKNOW=`md5sum $FILE`
 
+	# If the file has changed, change the md5 and create a new diff pdf
 	if ! [[ $CHECK = $CHECKNOW ]]
 	then
 		CHECK=$CHECKNOW
@@ -39,6 +43,9 @@ do
 		$LATEXDIFFCMD $ORIGINAL $FILE > $DIFF
 		$LATEXCMD $DIFF --output-directory $DIRECTORY
 	fi
+
 	sleep $TIME
+
+	# Exit on CTRL-C
 	test $? -gt 128
 done
